8fcf0ea4d7f4197734ffb9083946235104230551
Mike Ryan <mike@fadedink.co.uk>
2011-12-20 04:31:04 -0800

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-debug-toolbar',
-    version='0.9.7-dev',
+    version='0.9.8-dev',
     description='A configurable set of panels that display various debug information about the current request/response.',
     long_description=open('README.rst').read(),
     # Get more strings from http://www.python.org/pypi?:action=list_classifiers

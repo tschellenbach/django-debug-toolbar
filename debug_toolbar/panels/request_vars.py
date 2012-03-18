9352123a51e3eb028c6bcf31874e8333056876e1
WoLpH <Rick@Fawo.nl>
2012-03-18 23:18:31 +0100

    added view name and tag to request vars

--- debug_toolbar/panels/request_vars.py
+++ debug_toolbar/panels/request_vars.py
@@ -1,7 +1,10 @@
+import inspect
 from django.utils.translation import ugettext_lazy as _
 
 from debug_toolbar.panels import DebugPanel
 from debug_toolbar.utils import get_name_from_obj
+from django.core.urlresolvers import resolve
+from framework.utils import urlresolvers
 
 class RequestVarsDebugPanel(DebugPanel):
     """
@@ -33,6 +36,56 @@ class RequestVarsDebugPanel(DebugPanel):
         self.view_func = view_func
         self.view_args = view_args
         self.view_kwargs = view_kwargs
+
+    def get_view(self, request):
+        out = []
+        if self.request.path.startswith('/dev'):
+            '''Strip /dev'''
+            path = self.request.path[4:]
+        else:
+            path = self.request.path
+
+        out.append(('path', path))
+        out.append(('full_path', self.request.get_full_path()))
+
+        try:
+            name, namespace = urlresolvers.resolve_to_name(path)
+            function, args, kwargs = resolve(path)
+        except urlresolvers.Resolver404:
+            name, namespace = None, None
+            function, args, kwargs = None, (), {}
+
+        out.append(('view name', name))
+        if function:
+            function = getattr(function, 'wrapped_func', function)
+            filename = inspect.getfile(function)
+            line_no = inspect.getsourcelines(function)[-1]
+            out.append(('view func', '%s on line %d in %s' % (
+                function.__name__, line_no, filename)))
+
+        if name:
+            full_name = name
+            if namespace:
+                full_name = namespace + ':' + str(full_name)
+            
+            url_parts = ['{% url', full_name]
+
+            if args:
+                url_parts += map(unicode, args)
+
+            if kwargs:
+                for k, v in kwargs.iteritems():
+                    if isinstance(v, unicode):
+                        v = v.encode('utf-8', 'replace')
+                    url_parts.append('%s=%r' % (k, v))
+
+            url_parts.append('%}')
+            out.append(('view url ', ' '.join(url_parts)))
+        
+        else:
+            out.append(('view url ', name))
+
+        return out
     
     def process_response(self, request, response):
         self.record_stats({
@@ -48,6 +101,7 @@ class RequestVarsDebugPanel(DebugPanel):
                 name = '<no view>'
             
             self.record_stats({
+                'view': self.get_view(request),
                 'view_func': name,
                 'view_args': self.view_args,
                 'view_kwargs': self.view_kwargs
